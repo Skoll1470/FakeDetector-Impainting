ec5b3cb0976658ea3f8885946ae4c1261aa4617d
Skoll1470 <92733410+Skoll1470@users.noreply.github.com>
2022-03-08 09:27:25 +0100

    Add files via upload

---
+++ test_grey.cpp
@@ -0,0 +1,34 @@
+œ// test_couleur.cpp : Seuille une image en niveau de gris
+
+#include <stdio.h>
+#include "image_ppm.h"
+
+int main(int argc, char* argv[])
+{
+  char cNomImgLue[250], cNomImgEcrite[250];
+  int nH, nW, nTaille;
+  
+  if (argc != 4) 
+     {
+       printf("Usage: ImageIn.pgm ImageOut.pgm Seuil \n"); 
+       exit (1) ;
+     }
+   
+   sscanf (argv[1],"%s",cNomImgLue) ;
+   sscanf (argv[2],"%s",cNomImgEcrite);
+
+   OCTET *ImgIn, *ImgOut;
+   
+   lire_nb_lignes_colonnes_image_pgm(cNomImgLue, &nH, &nW);
+   nTaille = nH * nW;
+  
+   allocation_tableau(ImgIn, OCTET, nTaille);
+   lire_image_pgm(cNomImgLue, ImgIn, nH * nW);
+   allocation_tableau(ImgOut, OCTET, nTaille);
+	
+   
+
+   ecrire_image_pgm(cNomImgEcrite, ImgOut,  nH, nW);
+   free(ImgIn);
+   return 1;
+}
